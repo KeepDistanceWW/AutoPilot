1de1738c2eb1bbcf7aba16b7283b5d71fd529066
KeepDistanceWW <kaverin.semen@gmail.com>
2025-06-09 19:35:28 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         AutoPilot
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      3
 // @description  Автоимпорт карточек
+// @author       anonim
 // @match        https://sellerpilot.ru/catalog-v2*
 // @updateURL    https://github.com/KeepDistanceWW/AutoPilot/raw/refs/heads/main/script.user.js
 // @downloadURL  https://github.com/KeepDistanceWW/AutoPilot/raw/refs/heads/main/script.user.js
+// @grant        none
+// ==/UserScript==
 
 // no no no mister fish
